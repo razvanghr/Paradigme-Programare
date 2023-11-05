57ddba93b9d5e0aca6338f380cbab75ac33ffdd0
Razvan Ghilea <117529480+razvanghr@users.noreply.github.com>
2023-11-05 15:04:56 +0200

    fix

--- Laborator4/src/main/java/Problema4/Problema4.java
+++ Laborator4/src/main/java/Problema4/Problema4.java
@@ -2,10 +2,7 @@ package Problema4;
 
 public class Problema4 {
     public static void main(String[] args) {
-        Rectangle patrat = new Rectangle(4, 4);
         Rectangle dreptughi = new Rectangle(3 ,  2);
-
-        System.out.println("Patrat => Arie: " + patrat.arie() + " Perimetru: " + patrat.perimetru());
         System.out.println("Dreptunghi => Arie: " + dreptughi.arie() + " Perimetru: " + dreptughi.perimetru());
     }
 }
